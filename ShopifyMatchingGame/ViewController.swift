eb3daece75065c4763917e587458c1f75be18d49
Kingsley Chu <kingsleychu0730@hotmail.com>
2020-01-04 07:00:27 -0500

    Successfully decoded JSON

--- ShopifyMatchingGame/ViewController.swift
+++ ShopifyMatchingGame/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var response:Response?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
+        
+        let url = URL(string: "https://shopicruit.myshopify.com/admin/products.json?page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6")!
+        
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            guard let data = data else { return }
+            do {
+                let str = String(decoding: data, as: UTF8.self)
+                let jsonData = str.data(using: .utf8)!
+                let jsonResponse = try! JSONDecoder().decode(Response.self, from: jsonData)
+                
+                self.response = jsonResponse
+
+            } catch let error as NSError {
+                print("Failed to load: \(error.localizedDescription)")
+            }
+        }
+        
+        task.resume()
     }
 
 
